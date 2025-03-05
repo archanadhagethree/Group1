8c85bafca6e4ea330c143c7127e3b0b17d6d83b6
archanadhagethree <enggarchu07@gmail.com>
2025-03-05 20:47:38 +0000

    DecimalToHexTest changes

--- testDec2Hex.py
+++ testDec2Hex.py
@@ -3,25 +3,24 @@ from io import StringIO
 import sys
 
 
-class TestDecimalToHex(unittest.TestCase):
+class DecimalToHexTest(unittest.TestCase):
 
     def test_decimal_to_hex(self):
-        """Test conversion of decimal to hexadecimal."""
-        # Test for valid decimal numbers
-        self.assertEqual(decimal_to_hex(255), 'FF')  # 255 -> 'FF'
-        self.assertEqual(decimal_to_hex(10), 'A')    # 10 -> 'A'
+        # Test for valid decimal to hex numbers
         self.assertEqual(decimal_to_hex(0), '0')     # 0 -> '0'
         self.assertEqual(decimal_to_hex(1), '1')     # 1 -> '1'
+        self.assertEqual(decimal_to_hex(255), 'FF')  # 255 -> 'FF'
+        self.assertEqual(decimal_to_hex(10), 'A')    # 10 -> 'A'
 
     def test_invalid_input(self):
-        """Test invalid input that should raise ValueError."""
-        sys.argv = ['script.py', 'abc']  # Simulate invalid input (non-integer)
+        #Test invalid input with error
+        sys.argv = ['script.py', 'xyz']
         with self.assertRaises(ValueError):
             decimal_to_hex(int(sys.argv[1]))
 
     def test_no_input(self):
-        """Test if no input argument is provided."""
-        sys.argv = ['script.py']  # No argument passed
+        #Test if no input argument is given
+        sys.argv = ['script.py'] 
         with self.assertRaises(SystemExit):
             decimal_to_hex(int(sys.argv[1]))  # Should exit
 
