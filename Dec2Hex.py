c9a28b2a4bcad8cfcbbd77de166c57f984b4c6e5
archanadhagethree <enggarchu07@gmail.com>
2025-03-09 03:43:03 +0000

    Update Dec2Hex.py

--- Dec2Hex.py
+++ Dec2Hex.py
@@ -5,7 +5,7 @@ def decimal_to_hex(decimal_value):
         return "0"
 
     hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-    hexadecimal = 0
+    hexadecimal = ""
     num = decimal_value
 
     while num != 0:
