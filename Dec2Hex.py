ba141c3fc48051c5f11ab1eb63abe8135657f05e
archanadhagethree <enggarchu07@gmail.com>
2025-03-09 02:44:59 +0000

    Update Dec2Hex.py

--- Dec2Hex.py
+++ Dec2Hex.py
@@ -1,7 +1,7 @@
 import sys
 
 def decimal_to_hex(decimal_value):
-   if decimal_value == 0:
+    if decimal_value == 0:
         return "0"
 
     hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
@@ -15,7 +15,6 @@ def decimal_to_hex(decimal_value):
         hexadecimal = hex_chars[rem] + hexadecimal
         num //= 16
 
-
     print(f"Hexadecimal representation is : {hexadecimal}")
 
     return hexadecimal
@@ -25,7 +24,7 @@ if __name__ == "__main__":
         try:
             decimal_value = int(sys.argv[1])
             verbose = '--verbose' in sys.argv
-            hexadecimal = decimal_to_hex(decimal_value, verbose)
+            hexadecimal = decimal_to_hex(decimal_value)
             print(f"Hexadecimal: {hexadecimal}")
         except ValueError:
             print("Please provide a valid integer.")
