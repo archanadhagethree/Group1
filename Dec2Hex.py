b55b594d69ec98868c2756f6a8329178ece5e63e
archanadhagethree <enggarchu07@gmail.com>
2025-03-09 02:47:50 +0000

    Update Dec2Hex.py

--- Dec2Hex.py
+++ Dec2Hex.py
@@ -7,7 +7,6 @@ def decimal_to_hex(decimal_value):
     hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     hexadecimal = ""
     num = abs(decimal_value)
-    print(f"Converting the Decimal Value {num} to Hex...")
 
     while num != 0:
         rem = num % 16
