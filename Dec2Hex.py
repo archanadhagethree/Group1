533e98fddd4716c0042a4786f7493bfecf32d569
Ubuntu <ubuntu@ip-172-31-40-5.ec2.internal>
2025-03-10 21:17:29 +0000

    Print Changes

--- Dec2Hex.py
+++ Dec2Hex.py
@@ -24,5 +24,4 @@ def decimal_to_hex(decimal_value):
 if __name__ == "__main__":
     decimal_value = int(sys.argv[1])
     hexadecimal = decimal_to_hex(decimal_value)
-    print(f"Converting the Decimal Value {decimal_value} to Hex...")
-    print(f"Hexadecimal representation is: {hexadecimal}")
+    print(f"Converting the Decimal Value {decimal_value} to Hex.\n Hexadecimal representation is: {hexadecimal}")
